8f766350dd24433169fecffbfb54dd598cc90d34
samTop123 <barsam618@gmail.com>
2025-03-20 19:04:09 +0200

    In public

---
+++ UserFiles/script410.sql
@@ -0,0 +1,19 @@
+/*USE tasks;*/
+
+/*CREATE TABLE Books (
+    BookID INT PRIMARY KEY AUTO_INCREMENT,
+    Title VARCHAR(255),
+    Author VARCHAR(255),
+    PublishedYear INT,
+    Genre VARCHAR(100),
+    AvailableCopies INT
+);*/
+
+/*INSERT INTO Books (Title, Author, PublishedYear, Genre, AvailableCopies)
+VALUES 
+    ('To Kill a Mockingbird', 'Harper Lee', 1960, 'Fiction', 5),
+    ('1984', 'George Orwell', 1949, 'Dystopian', 3),
+    ('The Great Gatsby', 'F. Scott Fitzgerald', 1925, 'Classic', 4),
+    ('Moby Dick', 'Herman Melville', 1851, 'Adventure', 2);*/
+
+/*DROP TABLE Books;*/
